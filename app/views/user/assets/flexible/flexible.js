1b5473626501eed4454188d3d4a6b27e23975eb6
Maksim Gayduk <maksim.gayduk@gmail.com>
2019-01-09 12:13:31 +0200

    Created frame and points for flexible.js
    Wow variables in pure css :)

--- app/views/user/assets/flexible/flexible.js
+++ app/views/user/assets/flexible/flexible.js
@@ -3,9 +3,44 @@
 
     function TransformableElement(element) {
 
+        (function () {
+            let frame = createFrame();
+            element.parentElement.appendChild(frame);
+            frame.appendChild(element);
+
+        })();
+
         element.onclick = function() {
             console.log('onclicked troll');
         };
+
+        function createFrame() {
+            let frame = document.createElement('div');
+            frame.classList.add(`${pluginName}__frame`);
+            createPoints(frame);
+
+            return frame;
+        }
+
+        function createPoints(frame) {
+            let prefix = pluginName + '__position-';
+
+            let positionClasses = [
+                'top-left', 'top-center', 'top-right',
+                'middle-left', 'middle-right',
+                'bottom-left', 'bottom-center', 'bottom-right',
+            ];
+
+            for (let i = 0; i < positionClasses.length; i++) {
+                let point = document.createElement('div');
+                point.classList.add(
+                    'flexible__point',
+                    prefix + positionClasses[i],
+                );
+
+                frame.appendChild(point);
+            }
+        }
     }
 
     HTMLElement.prototype[pluginName] = function () {
